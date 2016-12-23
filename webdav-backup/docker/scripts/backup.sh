02f65c17490fe87bb9d02af1a48a0323733ba45f
Louis Fradin <louis.fradin@gmail.com>
2016-12-23 15:22:09 +0100

    Correct minor bugs

--- webdav-backup/docker/scripts/backup.sh
+++ webdav-backup/docker/scripts/backup.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
+# Get others variables
+. /docker/data/vars.sh
+
 # Variables
 NOW=`date +%Y%m%d-%H%M%S`
 ARCHIVE_DIR=/var/archives
 ARCHIVE_NAME="backup-${NOW}.tar.gz"
 BACKUP_DIR="backup-${NOW}"
 BACKUP_DIST_DIR="${WEBDAV_HOST}/${DESTINATION_FOLDER}/${BACKUP_DIR}"
-
-# Get others variables
-. /docker/data/vars.sh
 
 echo "#--------------# Start Backup #--------------#"
 
--- webdav-backup/docker/scripts/entrypoint.sh
+++ webdav-backup/docker/scripts/entrypoint.sh
@@ -5,6 +5,10 @@ if [ "$WEBDAV_HOST" == "" ]; then echo "Please provide a host for the webdav ope
 if [ "$WEBDAV_USERNAME" == "" ]; then echo "Please provide a username for the webdav operations"; exit; fi
 if [ "$WEBDAV_PASSWORD" == "" ]; then echo "Please provide a password for the webdav operations"; exit; fi
 
+# Eliminates quotes from some variables
+CRON_SCHEME=${CRON_SCHEME//\"}
+TIMEOUT=${TIMEOUT//\"}
+
 # Prepare file
 rm -f ~/.netrc
 echo "default" >> ~/.netrc
@@ -13,10 +17,16 @@ echo "password $WEBDAV_PASSWORD" >> ~/.netrc
 chmod 600 ~/.netrc
 
 # Create var file
+echo "WEBDAV_HOST=${WEBDAV_HOST}" >> /docker/data/vars.sh
+echo "WEBDAV_USERNAME=${WEBDAV_USERNAME}" >> /docker/data/vars.sh
+echo "WEBDAV_PASSWORD=${WEBDAV_PASSWORD}" >> /docker/data/vars.sh
 echo "ENCRYPTION=${ENCRYPTION}" >> /docker/data/vars.sh
 echo "ENCRYPTION_PASSWORD=${ENCRYPTION_PASSWORD}" >> /docker/data/vars.sh
+echo "SPLIT=${SPLIT}" >> /docker/data/vars.sh
+echo "SPLIT_SIZE=${SPLIT_SIZE}" >> /docker/data/vars.sh
 echo "BACKUP_NBR=${BACKUP_NBR}" >> /docker/data/vars.sh
 echo "DESTINATION_FOLDER=${DESTINATION_FOLDER}" >> /docker/data/vars.sh
+echo "TIMEOUT=${TIMEOUT}" >> /docker/data/vars.sh
 chmod 600 /docker/data/vars.sh
 
 # Create cron file
