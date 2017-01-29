0a0ce4848f7822af1994116dbf0d7ea97c0bb87c
Louis Fradin <louis.fradin@gmail.com>
2017-01-29 21:13:18 +0100

    Delete mongodb image

--- mongodb/docker/install/config.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-APTGET_INSTALL_ARGS="-y --force-yes --no-install-recommends"
-BUILD_PACKAGES=""
-RUN_PACKAGES="pwgen mongodb-org mongodb-org-server mongodb-org-shell mongodb-org-mongos mongodb-org-tools"
