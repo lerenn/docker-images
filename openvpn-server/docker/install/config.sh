25c03e575425c8b5b02d9ad2b7817d589f2228e4
Louis FRADIN <louis.fradin@gmail.com>
2018-12-02 14:16:59 +0100

    [all] Add curl installation

--- openvpn-server/docker/install/config.sh
+++ openvpn-server/docker/install/config.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 APTGET_INSTALL_ARGS="-y --no-install-recommends"
 BUILD_PACKAGES="git ca-certificates"
-RUN_PACKAGES="openssl openvpn iptables"
+RUN_PACKAGES="openssl openvpn iptables curl"
