68ba5a950d579634db011d46d667da37b4b33baa
Louis FRADIN <louis.fradin@gmail.com>
2020-03-22 16:59:02 +0100

    transmission-seedbox: correct htaccess creation bug

--- transmission-seedbox/docker/scripts/static-configuration.sh
+++ transmission-seedbox/docker/scripts/static-configuration.sh
@@ -10,3 +10,22 @@ ln -s /etc/apache2/sites-available/web.conf /etc/apache2/sites-enabled/web.conf
 
 # Activate htaccess modules
 a2enmod authz_groupfile
+
+# SFTP
+###############################################################################
+
+# Add configuration for SFTP
+cp /docker/configurations/sshd_config /etc/ssh/sshd_config
+
+# Add SFTP group
+addgroup sftp
+
+# Transmission
+###############################################################################
+
+# Create directories
+mkdir -p /data/configuration/torrents
+mkdir -p /data/downloads
+
+# Create file log
+touch /var/log/transmission.log
